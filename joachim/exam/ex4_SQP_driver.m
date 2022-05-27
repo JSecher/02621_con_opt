d87006b9b2bd6d88c8c2263877f0dc76b16bbbf0
JSecher <Joachim@Secher-fam.dk>
2022-05-27 17:52:07 +0200

    Rosenbrock

--- joachim/exam/ex4_SQP_driver.m
+++ joachim/exam/ex4_SQP_driver.m
@@ -5,8 +5,9 @@ close all
 
 runContourPlot_44 = false;
 runSolveTest_45= false;
+runSolveTest_452= true;
 runBFGS_46 = false;
-runBFGS_LS_47 = true;
+runBFGS_LS_47 = false;
 
 % Import CasADi
 if isfolder('../../casadi-v3.5.5')
@@ -180,6 +181,153 @@ savelatexTable(latex, input.tableLabel, 4);
 
 
 end
+
+%% Problem 4.5 - Solve Rosenbrock Part 1
+if runSolveTest_452
+%%
+disp("Solving Rosenbrock Test problem using fmincon and Casadi");
+
+% Select constraint for rosenbrok, 1 = circle, 2 = box
+con_case = 1;
+
+switch con_case
+    case 1
+        con_type_name = "Circle";
+        x0s = [[0.0; 0.0], [-0.5; 0], [-0.5; -0.5]];
+    case 2
+        con_type_name = "Box";
+        x0s = [[0.0; 1.0], [-1.25; 0.5], [-0.5; 1]];
+    otherwise
+        error("Unknown case for rosenbrock")
+end
+
+fig = figure("Name", sprintf("Rosenbrock - solutions for x0 %s constraints", con_type_name), 'Position', [100, 100, 800,800]);
+hold on
+% Create contour plot and constraints
+[cfig, conFigs] = contourRosen(con_case);
+
+for j=1:length(x0s)
+    % Define variables for casadi
+    x1 = SX.sym('x1');  % Define variables
+    x2 = SX.sym('x2');
+    
+    % Solve problem using fmincon
+    options = optimoptions('fmincon',... 
+                           'Display','none',... 
+                           'Algorithm','interior-point');
+
+    switch con_case
+        case 1
+            x0 = x0s(:,j);    % Initial point
+            xl = [-1; -1];      % Lower bound for x
+            xu = [1; 1];        % Upper bound for x
+            % For fmincon
+            tstart = cputime;
+            [sol_fmin,fval,exitflag,output] = fmincon(@objfunRosenbrock, ...
+                                                       x0, ...
+                                                       [], [], [], [], ...
+                                                       xl, xu, ...
+                                                       @confunRosenbrock, ...
+                                                       options);
+            time_fmincon = cputime - tstart;
+            
+            % For casadi
+            g = [x1^2+x2^2];
+        case 2
+            x0 = x0s(:,j);    % Initial point
+            xl = [-1.5; 0.5];      % Lower bound for x
+            xu = [1.5; 1.5];        % Upper bound for x
+            % fmincon
+            tstart = cputime;
+            [sol_fmin,fval,exitflag,output] = fmincon(@objfunRosenbrock, ...
+                                                       x0, ...
+                                                       [], [], [], [], ...
+                                                       xl, xu, ...
+                                                       @confunRosenbrock_part2, ...
+                                                       options);
+            time_fmincon = cputime - tstart;
+            % For casadi
+            g = [];
+        otherwise
+            error("Unknown case for rosenbrock")
+    end
+
+    % Define problem
+    hbprob = struct('x',[x1; x2], ...
+                    'f',100 * (x2-x1^2)^2 + (1-x1)^2, ...
+                    'g',g);
+    
+    % Solve the problem with casadi
+    options = struct;
+    options.ipopt.print_level = 0;
+    options.print_time = 0;
+    tstart = cputime;
+    S = nlpsol('S', 'ipopt', hbprob, options);
+    switch con_case
+        case 1
+            r = S('x0', x0, 'lbg',0,'ubg',1, 'lbx',-1,'ubx',1);
+        case 2
+            r = S('x0', x0, 'lbx',[-1.5, 0.5],'ubx',[1.5, 1.5]);
+    end
+    time_cas = cputime - tstart;
+    sol_cas = full(r.x);
+    obj_cas = full(r.f);
+    
+    % Print results
+    fprintf('Found solutions for x0 = [%.2f, %.2f] ::\n', x0(1), x0(2)); 
+    fprintf('\t fmincon solution: [%.5e, %.5e], objective: %.5e, time: %.5e\n', sol_fmin(1), sol_fmin(2), fval, time_fmincon);
+    fprintf('\t Casadi solution: [%.5e, %.5e], objective: %.5e, time: %.5e\n', sol_cas(1), sol_cas(2), obj_cas, time_cas);
+    
+    data(j,:) = [fval, sol_fmin', time_fmincon, nan, ...
+                 obj_cas, sol_cas', time_cas, nan, mean(sqrt((sol_fmin-sol_cas).^2))];
+
+        
+    % Add points of interest
+    fm = traceIterations([x0, sol_fmin], "r", '-');
+    cas = traceIterations([x0, sol_cas], "g");
+    intp = plotPoint(x0(1),x0(2), "int");
+    solp_fm = plotPoint(sol_fmin(1),sol_fmin(2), "sol", "r", 18);
+    solp_cas = plotPoint(sol_cas(1),sol_cas(2), "gen", "g", 8);
+end
+
+legend([intp,solp_fm,solp_cas, fm, cas, conFigs(1)],{'Initial Point', 'Fmincon solution', 'CasADi solution', 'Fmincon x_0 to sol.', 'CasADix_0 to sol.','Infeasible region'})
+hold off
+savefigpdf(fig, sprintf('ex4_solve_x0s_test_rosen_%s', lower(con_type_name)), 4);
+
+input.data = data';
+% Set column labels (use empty string for no label):
+input.tableColLabels = sprintfc("$x_0=[%.1f, %.1f]$", x0s')';
+% Set row labels (use empty string for no label):
+input.tableRowLabels = {'$f(x)_{\textit{fmincom}} =$', ...
+                        '$x_{\textit{fmincom}} = $', '', ...
+                        '$\text{time}_{\textit{fmincom}}\, [s] =$', ...
+                        '', ...
+                        '$f(x)_{\textit{CasADi}} =$', ...
+                        '$x_{\textit{CasADi}} = $', '', ...
+                        '$\text{time}_{\textit{CasADi}}\, [s] =$', ...
+                        '', 'Mean Squared Difference = '};
+% Set the row format of the data values 
+%input.dataFormatMode = 'row';
+input.dataFormat = {'%.4f'};
+% Column alignment ('l'=left-justified, 'c'=centered,'r'=right-justified):
+input.tableColumnAlignment = 'r';
+% Switch table borders on/off:
+input.booktabs = 1;
+% LaTex table caption:
+input.tableCaption = sprintf('Found solution for different inital points for the Rosenbrock test problem with %s constraint .', lower(con_type_name));
+% LaTex table label:
+input.tableLabel = sprintf('ex4_solve_test_rosen_%s', lower(con_type_name));
+input.makeCompleteLatexDocument = 0;
+input.dataNanString = '';
+input.tablePlacement = '!ht';
+% Now call the function to generate LaTex code:
+latex = latexTable(input);
+savelatexTable(latex, input.tableLabel, 4);
+
+
+end
+
+
 %% Problem 4.6 - Dampend BFGS
 if runBFGS_46 
 
@@ -442,6 +590,74 @@ function [c,ceq,dcdx,dceqdx] = confungradHimmelblau1(x,p)
         dcdx(2,1) = 1.0; % dc1dx2
         dcdx(1,2) = 4; % dc1dx1
         dcdx(2,2) = -10; % dc1dx2
+    end
+end
+
+function f = objfunRosenbrock(x,p)
+    % Function giving the objective of the Rosenbroc problem,
+    % objective function
+    tmp1 = (x(2)-x(1)*x(1));
+    tmp2 = tmp1*tmp1;
+    f = 100 * tmp2 + (1-x(1))*(1-x(1));
+end
+
+function [f,dfdx] = objfungradRosenbrock(x,p)
+    % Function giving the constraints and gradients of Rosenbroc problem,
+    % objective function
+    tmp1 = (x(2)-x(1)*x(1));
+    tmp2 = tmp1*tmp1;
+    f = 100 * tmp2 + (1-x(1))*(1-x(1));
+    % compute the gradient of f
+    if nargout > 1
+        dfdx = zeros(2,1);
+        dfdx(1,1) = -400*(-x(1)^2 + x(2))*x(1) - 2 + 2*x(1);
+        dfdx(2,1) = -200*x(1)^2 + 200*x(2);
+    end
+end
+
+function [c,ceq] = confunRosenbrock(x,p)
+    % Function giving the constraints of Rosenbrock problem,
+    % constraints 1, Unit circle
+    c = zeros(1,1);
+    ceq = zeros(0,1);
+
+    % Inequality constraints c(x) <= 0
+    c(1,1) = x(1)^2 + x(2)^2 - 1;
+end
+
+function [c,ceq] = confunRosenbrock_part2(x,p)
+    % Function giving the constraints of Rosenbrock problem,
+    % constraints 2, Box
+    c = zeros(0,1);
+    ceq = zeros(0,1);
+end
+
+function [c,ceq,dcdx,dceqdx] = confungradRosenbrock(x,p) 
+    % Function giving the constraints and gradients of Rosenbroc problem,
+    % constraints 1, unit circle
+    c = zeros(1,1);
+    ceq = zeros(0,1);
+    % Inequality constraints c(x) <= 0 
+    c(1,1) = x(1)^2 + x(2)^2 - 1;
+    % Compute constraint gradients
+    if nargout > 2
+        dcdx = zeros(2,1); 
+        dceqdx = zeros(2,0);
+        dcdx(1,1) = 2*x(1); % dc1dx1
+        dcdx(2,1) = 2*x(2); % dc1dx2
+    end
+end
+
+function [c,ceq,dcdx,dceqdx] = confungradRosenbrock_part2(x,p) 
+    % Function giving the constraints and gradients of Rosenbroc problem,
+    % constraints 2, Box
+    c = zeros(0,1);
+    ceq = zeros(0,1);
+    % Inequality constraints c(x) <= 0 
+    % Compute constraint gradients
+    if nargout > 2
+        dcdx = zeros(2,1); 
+        dceqdx = zeros(2,0);
     end
 end
 
